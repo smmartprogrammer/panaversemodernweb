646cf3ea5f1a9114d75f7aec274175067fb7b617
smmartprogrammer <partabrajp@gmail.com>
2023-03-10 03:14:13 +0500

    about page uploaded to git

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,22 +1,36 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
 import { TypingText } from "../components";
 import styles from "../styles";
 import { fadeIn, staggerContainer } from "../utils/motion";
 
-
 const About = () => (
-  <section className={`${styles.paddings} relative z-10`} >
+  <section className={`${styles.paddings} relative z-10`}>
     <div className="gradient-02 z-0" />
     <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: false, amount:0.25 }}
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-
+      <TypingText tittle="| About Panaverse" textStyles="text-center" />
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white "
+      >
+        The Future of the Web is Web 3.0, Metaverse, and Edge Computing.
+        Panaverse DAO is a movement to spread these technolgies globally. It is
+        community of Web 3 and Metaverse developers, designers, trainers,
+        startup founders and service providers.
+      </motion.p>
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow-down"
+        className="w-[18px] h-[28px] object-contain mt-[28px] "
+      />
     </motion.div>
   </section>
 );
