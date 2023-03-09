02122407717107e67492e94a8aaae015c8f17c2d
smmartprogrammer <partabrajp@gmail.com>
2023-03-10 02:13:03 +0500

    working on custom texts

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,23 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from "framer-motion";
+import {textContainer, textVariant2 } from "../utils/motion";
+
+export const TypingText = ({tittle, textStyles}) => (
+<motion.p 
+variants={textContainer}
+className={`font-normal text-[14px]
+text-secondary-white ${textStyles} `}
+>
+{Array.from(tittle).map((letter,  index) => (
+  <motion.span variants={textVariant2} key={index}>
+    {letter ===  ' ' ? '\u00A0' : letter }
+  </motion.span>
+) )}
+
+</motion.p>
 );
 
-export const TitleText = () => (
+export const TitleText = ({ tittle, textStyles }) => (
   <h2>Title Text</h2>
 );
