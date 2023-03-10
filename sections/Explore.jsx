7d3f75547ea0a6061afd083e2da36d6d8c648e84
smmartprogrammer <partabrajp@gmail.com>
2023-03-11 02:56:53 +0500

    working on explore card

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -7,7 +7,10 @@ import { exploreWorlds } from "../constants";
 import styles from "../styles";
 import { staggerContainer } from "../utils/motion";
 
-const Explore = () => (
+const Explore = () => {
+ const [active, setActive] = useState('world-2')
+ 
+  return (
   <section className={`${styles.paddings}`} id="explore">
     <motion.div
       variants={staggerContainer}
@@ -17,9 +20,28 @@ const Explore = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <TypingText tittle="| Panaverse Dao" textStyles="text-center" />
-      <TitleText tittle={ <>Choose the world you want <br className="md:block hidden"/> to explore</> } textStyles="text-center" />
+      <TitleText
+        tittle={
+          <>
+            Choose the world you want <br className="md:block hidden" /> to
+            explore
+          </>
+        }
+        textStyles="text-center"
+      />
+      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+        {exploreWorlds.map((world, index) => (
+          <ExploreCard
+            key={world.id}
+            {...world}
+            index={index}
+            active={active}
+            handleClick={setActive}
+          />
+        ))}
+      </div>
     </motion.div>
   </section>
-);
+)};
 
 export default Explore;
