27757e8b869af6bb3e98bbfcd4fa18d6d8f6ed69
smmartprogrammer <partabrajp@gmail.com>
2023-03-13 02:56:03 +0500

    website completed

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,29 +1,28 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
-import { textContainer, textVariant2 } from "../utils/motion";
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TypingText = ({ tittle, textStyles }) => (
+export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    className={`font-normal text-[14px]
-text-secondary-white ${textStyles} `}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
-    {Array.from(tittle).map((letter, index) => (
+    {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
-        {letter === " " ? "\u00A0" : letter}
+        {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
     ))}
   </motion.p>
 );
 
-export const TitleText = ({ tittle, textStyles }) => (
+export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
     initial="hidden"
     whileInView="show"
-    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles} `}
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
-    {tittle}
+    {title}
   </motion.h2>
 );
